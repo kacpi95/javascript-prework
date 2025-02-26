cedb17f7cb53971e3fe5508e6052e63e98e58c60
Kacper <kacpergrzywacz2@gmail.com>
2025-02-26 14:10:01 +0100

    Add functions game from script.js

--- js/script.js
+++ js/script.js
@@ -28,3 +28,29 @@ if (playerInput == "1") {
 }
 
 printMessage("Twój ruch to: " + playerMove);
+
+if (computerMove == "kamień" && playerMove == "papier") {
+	printMessage(`Ty wygrywasz!`);
+} else if (computerMove == "kamień" && playerMove == "kamień") {
+	printMessage("Remis");
+} else if (computerMove == "kamień" && playerMove == "nożyce") {
+	printMessage("Computer wygrał");
+} else if (computerMove == "papier" && playerMove == "nożyce") {
+	printMessage(`Ty wygrywasz!`);
+} else if (computerMove == "papier" && playerMove == "papier") {
+	printMessage("Remis");
+} else if (computerMove == "papier" && playerMove == "kamień") {
+	printMessage("Computer wygrał");
+} else if (computerMove == "nożyce" && playerMove == "kamień") {
+	printMessage(`Ty wygrywasz!`);
+} else if (computerMove == "nożyce" && playerMove == "nożyce") {
+	printMessage("Remis");
+} else if (computerMove == "nożyce" && playerMove == "papier") {
+	printMessage("Computer wygrał");
+} else if (
+	computerMove == "papier" ||
+	computerMove == "kamień" ||
+	(computerMove == "nożyce" && playerMove == "nieznany ruch")
+) {
+	printMessage("Runda jest nieważna");
+}
