2be773d60675d13c13d4f6f16402259ce0328878
Kacper <kacpergrzywacz2@gmail.com>
2025-02-28 18:59:14 +0100

    Add listner to script.js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,51 @@
-const getMoveName = (moveId) => {
-	if (moveId == 1) {
-		return "kamień";
-	} else if (moveId == 2) {
-		return "papier";
-	} else if (moveId == 3) {
-		return "nożyce";
-	} else {
-		return "nieznany ruch";
-	}
-};
+const btnRock = document.querySelector(".play-rock");
+const btnPaper = document.querySelector(".play-paper");
+const btnScissors = document.querySelector(".play-scissors");
 
-const displayResult = (computerMove, playerMove) => {
-	if (
-		(computerMove == "kamień" && playerMove == "nożyce") ||
-		(computerMove == "papier" && playerMove == "kamień") ||
-		(computerMove == "nożyce" && playerMove == "papier")
-	) {
-		printMessage("Komputer wygrał");
-	} else if (computerMove === playerMove) {
-		printMessage("Remis");
-	} else {
-		printMessage("Ty wygrywasz");
-	}
-};
+function playGame(playerInput) {
+	clearMessages();
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-let computerMove = getMoveName(randomNumber);
-let playerInput = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
-let playerMove = getMoveName(playerInput);
+	const getMoveName = (moveId) => {
+		if (moveId == 1) {
+			return "kamień";
+		} else if (moveId == 2) {
+			return "papier";
+		} else if (moveId == 3) {
+			return "nożyce";
+		} else {
+			return "nieznany ruch";
+		}
+	};
 
-printMessage("Mój ruch to: " + computerMove);
-printMessage("Twój ruch to: " + playerMove);
+	const displayResult = (computerMove, playerMove) => {
+		if (
+			(computerMove == "kamień" && playerMove == "nożyce") ||
+			(computerMove == "papier" && playerMove == "kamień") ||
+			(computerMove == "nożyce" && playerMove == "papier")
+		) {
+			printMessage("Komputer wygrał");
+		} else if (computerMove === playerMove) {
+			printMessage("Remis");
+		} else {
+			printMessage("Ty wygrywasz");
+		}
+	};
 
-displayResult(computerMove, playerMove);
+	let randomNumber = Math.floor(Math.random() * 3 + 1);
+	let computerMove = getMoveName(randomNumber);
+	let playerMove = getMoveName(playerInput);
+
+	printMessage("Mój ruch to: " + computerMove);
+	printMessage("Twój ruch to: " + playerMove);
+
+	displayResult(computerMove, playerMove);
+}
+btnRock.addEventListener("click", function () {
+	playGame(1);
+});
+btnPaper.addEventListener("click", function () {
+	playGame(2);
+});
+btnScissors.addEventListener("click", function () {
+	playGame(3);
+});
